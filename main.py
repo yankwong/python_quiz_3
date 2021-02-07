5e2c8ad451fedb8fce6b6d9b4334258aa80b8998
Andy Kwong <yantkwong@gmail.com>
2021-02-07 13:40:09 -0800

    added question 3

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # Please explain the functionality of each block of codes/questions
+print('-- question 1 --')
 import question_1
+print('-- question 2 --')
 import question_2
+print('-- question 3 --')
+import question_3
 
 if __name__ == '__main__':
     print('-- end of quiz 3 --')
---
+++ question_3.py
@@ -0,0 +1,9 @@
+# create a dictionary with two items.
+# Each item contain a key (string) and a value (list of string)
+projects = {"Project1": ["Sam", "Tom"], "Project2": ["Tim", "Cooper"]}
+# print out the keys of the dictionary
+print(projects.keys())
+# print out all items of the dictionary
+print(projects.items())
+# print out the values of each item in the dictionary
+print(projects.values())
